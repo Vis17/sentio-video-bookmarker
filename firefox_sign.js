0fa1eefd8eca9c35171cbe39b7e71be9ce650c3b
Vis17 <93092155+Vis17@users.noreply.github.com>
2022-01-03 20:52:47 +0100

    Update `firefox_sign.js`: move id in external `.json` file

--- firefox_sign.js
+++ firefox_sign.js
@@ -1,10 +1,10 @@
 /* eslint-disable */
 
 const { exec } = require('child_process');
-const { api_key, api_secret } = require('./firefox_api_creds.json');
+const { api_key, api_secret, id } = require('./firefox_api_creds.json');
 
 exec(
-	`npx web-ext sign -s ./dist -a ./web-ext-artifacts --api-key ${api_key} --api-secret ${api_secret} --id {8213fe4e-f8a6-4415-9e0a-0426ad36dfcd}`,
+	`npx web-ext sign -s ./dist -a ./web-ext-artifacts --api-key ${api_key} --api-secret ${api_secret} --id "${id}"`,
 	(error, stdout, stderr) => {
 		if (error) return console.log(`error: ${error.message}`);
 		if (stderr) return console.log(`stderr: ${stderr}`);
