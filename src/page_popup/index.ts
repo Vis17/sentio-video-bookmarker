df5ac1d55d66e83837a3b26254668ddd1c29c770
Vis17 <93092155+Vis17@users.noreply.github.com>
2022-01-23 10:10:21 +0100

    Remove `page_popup`
    - remove in `manifest.json`
    - remove in webpack config
    - remove now unnecessary `_popup.scss`

--- src/page_popup/index.ts
+++
@@ -1,9 +0,0 @@
-import 'svelte'; // avoid compiling errors
-import '../scss/index.scss';
-import Popup from './Popup.svelte';
-
-const popup = new Popup({
-	target: document.body,
-});
-
-export default popup;
